a7e2fe0e37b4615044b3ec973c77fed207e8d85a
nelson CFP <jhoeladarmes@gmail.com>
2022-06-10 12:04:01 -0300

    termine algoritmo en clase

--- algoritmo.js
+++ algoritmo.js
@@ -2,6 +2,9 @@ const PRECIO_CERVEZA = 400;
 const PRECIO_COCACOLA = 200;
 const PRECIO_LEVITE = 180;
 const DESCUENTO_POR_PAGO_EFECTIVO = 0.20;
+const INTERES_POR_PAGO_EN_3_CUOTAS = 0.05;
+const INTERES_POR_PAGO_EN_6_CUOTAS = 0.15;
+
 
 let cantidadCerveza = 0 //"cantidades de cerveza";
 let cantidadCocacola = 0//"cantidades de cocacola";
@@ -15,6 +18,10 @@ let total_levite = 0
 let total_levitePera = 0
 let subTotal = 0
 let TotalPago = 0
+let tipoDePago = "tipo de pago seleccionado por el usuario";
+let cantidadCuotas = "cantidad de cuotas seleccionado por el usuario";
+let recargoDescuento = "Recargo o descuento según tipo de pago y cuotas";
+
 
 let elementosInventario = 1;
 let elemento = "valor booleano";
@@ -132,7 +139,7 @@ function vaciarCarrito(){
 }
 
 function pagoTarjeta (){
-    
+    tipoDePago = "Tarjeta";
     if(cantidadTotalProductos < 3){
         
         document.querySelector("#pago_final").innerHTML=`
@@ -170,8 +177,9 @@ function pagoTarjeta (){
     }
 }
 function cuotasSeleccionadas(){
+    cantidadCuotas= (Number(document.querySelector("#cantidad_cuotas").value))
     calcularTotales()
-    switch (Number(document.querySelector("#cantidad_cuotas").value)){
+    switch (cantidadCuotas){
         case 1:
             descuentoPagoefectivo()
             document.querySelector("#pago_final").innerHTML =`
@@ -180,13 +188,17 @@ function cuotasSeleccionadas(){
                 `
             break
         case 3:
+            interesPagocuotas(INTERES_POR_PAGO_EN_3_CUOTAS);
             document.querySelector("#pago_final").innerHTML =`
+                <h4>Pagando en 3 cuotas tenés un ${INTERES_POR_PAGO_EN_3_CUOTAS*100}% de interés </h4>
                 <h4>Pagás 3 cuotas de ${totalPago / 3} cada una </h4>
                 <h4>En total pagarás $${totalPago}</h4>
                 `
             break    
         case 6:
+            interesPagocuotas(INTERES_POR_PAGO_EN_6_CUOTAS);
             document.querySelector("#pago_final").innerHTML =`
+                <h4>Pagando en 6 cuotas tenés un ${INTERES_POR_PAGO_EN_6_CUOTAS*100}% de interés </h4>
                 <h4>Pagás 6 cuotas de ${totalPago / 6} cada una </h4>
                 <h4>En total pagarás $${totalPago}</h4>
                 `
@@ -195,10 +207,19 @@ function cuotasSeleccionadas(){
     }
 
 }
+
+function interesPagocuotas(interesSegunCantidadCuotas){
+    totalPago = subTotal + (subTotal* interesSegunCantidadCuotas)
+    recargoDescuento = `Recargo ${interesSegunCantidadCuotas*100}%`
+}
+
 function descuentoPagoefectivo(){
      totalPago = subTotal - (subTotal * DESCUENTO_POR_PAGO_EFECTIVO)
+     recargoDescuento = `Dcto ${DESCUENTO_POR_PAGO_EFECTIVO * 100}%`;
 }
 function pagoEfectivo(){
+    tipoDePago = "Efectivo";
+    cantidadCuotas = "No aplica";
     descuentoPagoefectivo()
     document.querySelector("#pago_final").innerHTML=`
     <h4> Pagando con efectivo tenés un ${DESCUENTO_POR_PAGO_EFECTIVO * 100}% de descuento</h4>
@@ -220,3 +241,93 @@ function imprimirTablaCarrito(){
     `;
 }
 
+function imprimirTablaFactura(){
+    document.querySelector(`#factura`).innerHTML=``;
+    document.querySelector(`#factura`).innerHTML=`
+        <table id= "tabla_factura">
+            <tr id= "fila_cero_tabla_factura">
+                <td>Producto</td>
+                <td>Cantidad</td>
+                <td>Costo Unid.</td>
+                <td>Costo Total</td>
+                <td>Tipo de pago</td>
+                <td>Cuotas</td>
+                <td>Recargo/Descuento</td>
+            </tr>            
+        </table>
+
+    `;
+}
+
+function mostrarTotal(){
+    document.querySelector(`#totalTotal`).innerHTML = `<h4> Total= $${totalPago}</h4><br>`       
+}
+
+function confirmaCompra(){
+    
+    imprimirTablaFactura()    
+    for (i=1 ; i < elementosInventario ; i ++){        
+        if (i==1 & cantidadCerveza > 0 ){
+            escribirProductoEnFactura("Cerveza Litro",cantidadCerveza,PRECIO_CERVEZA,total_cerveza,tipoDePago,cantidadCuotas,recargoDescuento)
+        }
+        else if (i==2 & cantidadCocacola > 0){
+            escribirProductoEnFactura("Coca-Cola 2 Litros",cantidadCocacola,PRECIO_COCACOLA,total_cocacola,tipoDePago,cantidadCuotas,recargoDescuento)
+        }
+        else if (i==3 & cantidadLevite > 0){    
+            escribirProductoEnFactura("Levité Manzana Litro",cantidadLevite,PRECIO_LEVITE,total_levite,tipoDePago,cantidadCuotas,recargoDescuento)
+        }
+        else if (i==4 & cantidadLevitePera > 0){
+            escribirProductoEnFactura("Levité Pera Litro",cantidadLevitePera,PRECIO_LEVITE,total_levitePera,tipoDePago,cantidadCuotas,recargoDescuento)
+        }    
+        else {mostrarTotal()}
+        
+    };   
+}
+function escribirProductoEnFactura(nombreProducto,cantidadProducto,costoIndividual,costoTotal,tipoDePago,cuotas,recargo_Descuento){
+    generarFilaEnTablaFactura()
+    for(columna=1; columna < 8; columna ++){  
+        switch (columna){
+        case 1:
+            document.querySelector(`#fact_col_${columna}_fila${i}`).innerHTML +=`
+            ${nombreProducto}`
+            break;
+        case 2:
+            document.querySelector(`#fact_col_${columna}_fila${i}`).innerHTML +=`
+            ${cantidadProducto}`
+            break;
+        case 3:
+            document.querySelector(`#fact_col_${columna}_fila${i}`).innerHTML +=`
+            ${costoIndividual}`
+            break;
+        case 4:
+            document.querySelector(`#fact_col_${columna}_fila${i}`).innerHTML +=`
+            ${costoTotal}`
+            break;
+        case 5:
+            document.querySelector(`#fact_col_${columna}_fila${i}`).innerHTML +=`
+            ${tipoDePago}`
+            break;
+        case 6:
+            document.querySelector(`#fact_col_${columna}_fila${i}`).innerHTML +=`
+            ${cuotas}`
+            break;
+        case 7:
+            document.querySelector(`#fact_col_${columna}_fila${i}`).innerHTML +=`
+            ${recargo_Descuento}`
+            break; 
+        }
+    }
+}
+function generarFilaEnTablaFactura(){
+    document.querySelector("#tabla_factura").innerHTML+=`
+        <tr>
+            <td id= "fact_col_1_fila${i}"></td>
+            <td id= "fact_col_2_fila${i}"></td>
+            <td id= "fact_col_3_fila${i}"></td>
+            <td id= "fact_col_4_fila${i}"></td>
+            <td id= "fact_col_5_fila${i}"></td>
+            <td id= "fact_col_6_fila${i}"></td>
+            <td id= "fact_col_7_fila${i}"></td>
+        </tr>
+    `;
+}
